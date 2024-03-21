3b9ea2011ef43c122563114110424afb548ea21e
rots1 <ivairwork@outlook.com>
2024-03-21 13:50:11 -0400

    finalizado

--- taxa de compressao/calculoTaxa.py
+++ taxa de compressao/calculoTaxa.py
@@ -1,12 +1,8 @@
 
 import math
 
-volume_deslocamento = 0
-volume_comprimento = 0
-
 
 def calc_volume_cilindro(diamentro_cilindro):
-    pi = 3.14
     resultado_divisao = diamentro_cilindro/2
 
     parte_decimal = str(resultado_divisao).split('.')[1][:2]
@@ -34,34 +30,53 @@ def calc_volume_altura(area, altura):
     v = round(v)
     return v
 
-# vcab volume da camara de combustão
-# vdeck
-# vcam
 def volume_camara(area, alt, vcab):
     v = area * alt
     numero_formatado = f"{v:,.1f}"
     remove_pos_virgula = str(numero_formatado).split('.')[0]
     numero_sem_virgula = remove_pos_virgula.replace(",",".")
-    vdeck = f'{float(numero_sem_virgula):.1f}'
+    vdeck = f'{float(numero_sem_virgula):.2f}'
     vcam = float(vdeck) + float(vcab)
 
-    return vcam
+    return vcam, vdeck
 
 def cal_taxa_compressao(vcil, vcam):
     t = (vcil + vcam)/vcam
     return t
 
-diametro_cilindro = 90.45
+def alterar_volume_deck(taxa_ideal, vcil, vdeck):
+    # taxa = vcil + vdeck + vcab/ vdeck + vcab
+    taxa = taxa_ideal
+    vdeck = float(vdeck)
+    vcil = float(vcil)
+    vcab = taxa * vdeck
+    r = vcil + vdeck
+    v = r - vcab
+    print(f'alterar: {vcil}, {vcab}, {r}, {v}')
+    vcab = taxa - 1
+    resultado = v/vcab
+
+    return resultado
+
+   
+
+
+diametro_cilindro = 85.5
 altura_curso_cilindro  = 69
-altura_cilindro_ponto_morto_superior = 1.30
-volume_camara_combustao = 58
+altura_cilindro_ponto_morto_superior =2
+volume_camara_combustao = 34
+taxa_ideal = 9.5
 
 area = calc_volume_cilindro(diametro_cilindro)
 
 volume = calc_volume_altura(area, altura_curso_cilindro)
 
-vcam = volume_camara(area, altura_cilindro_ponto_morto_superior, volume_camara_combustao)
+vcam, vdeck = volume_camara(area, altura_cilindro_ponto_morto_superior, volume_camara_combustao)
 
 taxa = cal_taxa_compressao(volume, vcam)
 
-print(taxa)
+alterar = alterar_volume_deck(taxa_ideal, volume, vdeck)
+
+print(f'Volume cilindro: {volume}, Volume Camera: {vcam}, vdeck: {vdeck}')
+print(f'Taxa: {taxa}\n')
+print(f'Precisa alterar {alterar}')
